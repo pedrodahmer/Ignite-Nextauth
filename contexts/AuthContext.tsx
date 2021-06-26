25df04a3fc0dd67ffeb90c18a8f8219c7fd5879f
pedro dahmer <pedrohd1213@gmail.com>
2021-06-26 12:06:46 -0300

    botao de logout que encerra a sessao em todas as abas do app abertas

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -15,7 +15,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>;
+    signIn: (credentials: SignInCredentials) => Promise<void>;
+    signOut: () => void;
     user: User;
     isAuthenticated: boolean;
 }
@@ -28,9 +29,13 @@ export const AuthContext = createContext(
     {} as AuthContextData
 )
 
+let authChannel: BroadcastChannel
+
 export function signOut(){
     destroyCookie(undefined, 'nextauth.token')
     destroyCookie(undefined, 'nextauth.refreshToken')
+
+    authChannel.postMessage('signOut')
     
     Router.push('/')
 }
@@ -38,6 +43,21 @@ export function signOut(){
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>()
     const isAuthenticated = !!user
+
+    useEffect(() => {
+        authChannel = new BroadcastChannel('auth')
+        
+        authChannel.onmessage = (message) => {
+            switch (message.data) {
+                case 'signOut': 
+                    signOut()
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    }, [])
 
     useEffect(() => {
         const { 'nextauth.token': token } = parseCookies()
@@ -88,7 +108,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated, user}}>
+        <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user}}>
             {children}
         </AuthContext.Provider>
     )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
-    const { user } = useContext(AuthContext)
+    const { user, signOut } = useContext(AuthContext)
     
     useEffect(() => {
         api.get('/me')
@@ -17,6 +17,8 @@ export default function Dashboard(){
     return (
         <>
             <h1>Dashboard: {user?.email}</h1>
+
+            <button onClick={signOut}>Signout</button>
 
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
